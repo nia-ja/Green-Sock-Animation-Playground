6f9b6d314cf2375dd48d5b3a404455b14d2ef298
Kasia Bondarava <32349369+nia-ja@users.noreply.github.com>
2019-02-19 22:20:50 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,100 @@
+window.onload = function(){
+    TweenLite.to("#logo1", 10, {
+        x:600,
+        y:200,
+        rotation:360,
+        scale:0.5,
+        ease:Back.easeOut,
+        backgroundColor:"rgba(83, 180, 232, 0.5)",
+        borderRadius:"50%",
+        border:"5px solid lightgrey"
+    });
+    TweenLite.to("#logo2", 10, {
+        x:600,
+        y:-178.5,
+        rotation:360,
+        scale:0.5,
+        ease:Back.easeOut,
+        backgroundColor:"rgba(232, 82, 82, 0.4)",
+        borderRadius:"50%",
+        border:"5px solid lightgrey"
+    });
+    TweenLite.to("#logo3", 10, {
+        x:600,
+        rotation:720,
+        scale:0.3,
+        ease:Back.easeOut,
+        backgroundColor:"rgb(239, 225, 67, 0.9)",
+        borderRadius:"50%",
+        border:"5px solid lightgrey"
+    });
+    TweenLite.to("#logo4", 10, {
+        x:-500,
+        rotation:720,
+        scale:0.6,
+        ease:Back.easeOut,
+        backgroundColor:"rgba(82, 232, 101, 0.7)",
+        borderRadius:"50%",
+        border:"5px solid lightgrey"
+    });
+    TweenLite.to("#logo5", 10, {
+        x:-600,
+        y:100,
+        rotation:360,
+        scale:0.8,
+        ease:Back.easeOut,
+        backgroundColor:"rgba(82, 159, 232, 0.5)",
+        borderRadius:"50%",
+        border:"5px solid lightgrey"
+    });
+    TweenLite.to("#logo6", 10, {
+        x:-100,
+        y:-100,
+        rotation:360,
+        scale:0.3,
+        ease:Back.easeOut,
+        backgroundColor:"rgba(179, 82, 232, 0.5)",
+        borderRadius:"50%",
+        border:"5px solid lightgrey"
+    });
+    TweenLite.to("#logo7", 10, {
+        x:-600,
+        y:400,
+        rotation:1080,
+        scale:0.7,
+        ease:Back.easeOut,
+        backgroundColor:"rgba(179, 219, 50, 0.8)",
+        borderRadius:"50%",
+        border:"5px solid lightgrey"
+    });
+    TweenLite.to("#logo8", 10, {
+        x:400,
+        y:300,
+        rotation:360,
+        scale:0.7,
+        ease:Back.easeOut,
+        backgroundColor:"rgba(7, 193, 144, 0.9)",
+        borderRadius:"50%",
+        border:"5px solid lightgrey"
+    });
+    TweenLite.to("#logo9", 10, {
+        // x:-200,
+        y:-150,
+        rotation:360,
+        scale:0.9,
+        ease:Back.easeOut,
+        backgroundColor:"rgba(196, 56, 56, 0.95)",
+        borderRadius:"50%",
+        border:"5px solid lightgrey"
+    });
+    TweenLite.to("#logo10", 10, {
+        x:-10,
+        y:-100,
+        rotation:360,
+        scale:0.6,
+        ease:Back.easeOut,
+        backgroundColor:"rgba(232, 119, 13, 0.8)",
+        borderRadius:"50%",
+        border:"5px solid lightgrey"
+    });
+}
